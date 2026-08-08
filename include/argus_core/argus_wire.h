9004c3b7e9dc3f2b14c3d4a722da7e3142be9bab
Max-Gabriel-Susman <gabe.susman@gmail.com>
2026-08-08 02:52:22 -0600

    moved all the definitions out except for crc16_ccitt. we'll want to prevent drift in it's logic between argus_sensors and argus_safety_controller. having them both vendor it from this repo will enforce that

--- include/argus_core/argus_wire.h
+++ include/argus_core/argus_wire.h
@@ -1,43 +1,41 @@
-#include "lwip/udp.h"
-#include "lwip/init.h"
-#include "netif/xadapter.h"
+#ifndef ARGUS_CORE_ARGUS_WIRE_H
+    #define ARGUS_CORE_ARGUS_WIRE_H
 
-static struct udp_pcb *g_pcb;
-static ip_addr_t g_dest;
+    #include <stdint.h>
+    #include <assert.h>
 
-// cyclic redundancy check
-static uint16_t crc16_ccitt()
-{
-    uint16_t crc = 0xFFFF;
-    for (size_t i = 0; i < n; i++) {
-        crc ^= (uint16_t)d[i] << 8;
-        for (int b = 0; b < 8; b++) {
-            crc = (crc & 0x8000) ? (crc << 1) ^ 0x1021 : (crc << 1);
+    /* Wire contract betweeen the Zynq safety controller and argus_sensors.
+     * Both ends must agree. Bump ARGUS_FRAME_VERSION on any layout change. */
+
+    #define FRAME_MAGIC 0x41524753u /* 'A''R''G''S' */
+    #define FRAME_VERSION 1
+    #define MAX_CHANNELS 96
+    #define UDP_PORT 5005
+
+    typedef struct __attribute__((packed)) {
+        uint32_t magic;
+        uint32_t sample;
+        float    t;                      /* s since fw start */              
+        uint8_t  version;
+        uint8_t  reserved;
+        uint16_t channel_count;
+        uint16_t channels[MAX_CHANNELS];
+        uint16_t crc;                    /* CRC-16/CCITT bytes [0,crc) */
+    } argus_frame_packet_t;
+    
+    static_assert(sizeof(argus_frame_packet_t) == 210, "frame size drift");
+
+    
+    // cyclic redundancy check
+    static inline uint16_t crc16_ccitt(const uint8_t *d, size_t n)
+    {
+        uint16_t crc = 0xFFFF;
+        for (size_t i = 0; i < n; i++) {
+            crc ^= (uint16_t)d[i] << 8;
+            for (int b = 0; b < 8; b++) {
+                crc = (crc & 0x8000) ? (crc << 1) ^ 0x1021 : (crc << 1);
+            }
         }
+        return crc;
     }
-    return crc;
-}
-
-int argus_net_init(void)
-{
-    // Create a protocol control block; a handle for a udp endpoint.
-    g_pcb = udp_new(); 
-    if (!g_pcb) {
-        return -1;
-    }
-    IP4_ADDR(&g_dest, 192, 168, 1, 20); /* Orin */
-    return 0;
-}
-
-int argus_send_frame(uint32_t sample, float t, const uint16_t *ch)
-{
-    argus_frame_packet_t pkt = {0};
-    pkt.magic = ARGUS_FRAME_MAGIC;
-    pkt.sample = sample;
-    pkt.t = t;
-    pkt.version = ARGUS_FRAME_VERSION;
-    pkt.channel_count = ARGUS_MAX_CHANNELS;
-    memcpy(pkt.channels, ch, sizeof(pkt.channels));
-    pkt.crc = crc16_ccitt((const uint8_t *)&pkt, offsetof(argus_frame_packet_t, crc));
-    // not finished yet.
-}
+#endif /* ARGUS_CORE_ARGUS_WIRE_H */
